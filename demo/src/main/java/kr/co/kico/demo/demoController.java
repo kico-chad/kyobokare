2697abc4c5637ffa7a426df1fc2bea7103664599
asd <hcs1906@kico.co.kr>
2021-03-03 11:05:10 +0900

    testest

---
+++ demo/src/main/java/kr/co/kico/demo/demoController.java
@@ -0,0 +1,16 @@
+package kr.co.kico.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class demoController {
+	
+	@GetMapping("/test")
+	private void test() {
+		System.out.println("test");
+	}
+	
+}
